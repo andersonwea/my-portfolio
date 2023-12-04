d73d7447603577ca8729a00fa9407a10acafc7e6
chain <klaus.mika1052@gmail.com>
2023-12-04 13:08:39 -0300

    feat: add fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,21 @@ import type { Metadata } from 'next'
 import { Poppins, Lora } from 'next/font/google'
 import './globals.css'
 import { Providers } from './providers'
+import { Header } from './components/Header'
 
-const poppins = Poppins({ weight: ['400', '700'], variable: '--font-poppins' })
-const lora = Lora({ weight: ['400', '700'], variable: '--font-lora' })
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-poppins',
+})
+const lora = Lora({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-lora',
+})
 
 export const metadata: Metadata = {
-  title: 'AndersonWea',
+  title: 'AndersonWea | Portfolio',
   description: 'Portfolio de andersonwea',
 }
 
@@ -18,11 +27,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Providers>
-        <body className={`${poppins.variable} ${lora.variable}`}>
-          {children}
-        </body>
-      </Providers>
+      <body
+        className={`${poppins.variable} ${lora.variable} bg-bg-primary h-screen font-primary`}
+      >
+        <Providers>
+          <Header />
+          <div className="max-w-[1232px] mx-auto">{children}</div>
+        </Providers>
+      </body>
     </html>
   )
 }
