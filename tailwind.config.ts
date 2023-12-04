e799044cc76bb804ca4b5a9ba34fb46140f4d9de
chain <klaus.mika1052@gmail.com>
2023-12-04 10:40:39 -0300

    chore: add colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,11 +6,35 @@ const config: Config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#4DB5FF',
+        secondary: 'rgba(77, 181, 255, 0.4)',
+        light: 'rgba(255, 255, 255, 0.6)',
+        'bg-primary': '#1F1F38',
+        'bg-secondary': '#2C2C6C',
+      },
+    },
   },
-  plugins: [nextui()],
+  plugins: [
+    nextui({
+      themes: {
+        light: {
+          colors: {
+            primary: {
+              foreground: '#1F1F38',
+              DEFAULT: '#4DB5FF',
+            },
+            secondary: {
+              foreground: '#4DB5FF',
+            },
+          },
+        },
+      },
+    }),
+  ],
 }
 export default config
