94744684d7efef7f56221d9fc81d488c3f666615
chain <klaus.mika1052@gmail.com>
2023-12-04 13:07:59 -0300

    feat: create text component

---
+++ src/app/components/Text.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { twMerge } from 'tailwind-merge'
+
+interface TextProps {
+  children: string
+  className?: string
+  as?: 'p' | 'span'
+}
+
+export function Text({ children, className, as = 'span' }: TextProps) {
+  const styles = 'text-base text-light'
+
+  const elements = {
+    span: <span className={twMerge(styles, className)}>{children}</span>,
+    p: <p className={twMerge(styles, className)}>{children}</p>,
+  }
+
+  return elements[as]
+}
