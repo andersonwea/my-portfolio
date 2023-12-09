a3790a8081fee8fb26555f49a5b79c4b251da86b
chain <klaus.mika1052@gmail.com>
2023-12-09 14:41:34 -0300

    feat: create slider

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -1,10 +1,16 @@
-import Image from 'next/image'
+'use client'
+
 import { Title } from './Title'
-import CryptoFolio from '@/assets/projects/dashboard-edited.png'
+import { Projetc } from './Project'
+import React from 'react'
+import { useKeenSlider } from 'keen-slider/react'
+import 'keen-slider/keen-slider.min.css'
+import { ChevronLeft, ChevronRight } from 'lucide-react'
+import { Button } from '@nextui-org/react'
 import { Text } from './Text'
-import { Heading } from './Heading'
-import { Button, Chip, Link } from '@nextui-org/react'
-import { ExternalLinkIcon, Github } from 'lucide-react'
+import CryptoFolio from '@/assets/projects/dashboard-edited.png'
+import PlayAi from '@/assets/projects/plai-ai.png'
+import MineIt from '@/assets/projects/mine-it.png'
 
 const projects = [
   {
@@ -12,6 +18,7 @@ const projects = [
     description:
       'Um aplicativo web para gerenciar sua carteira de cryptomoedas e acompanhar dados como preços, volumes de negociação e capitalização de mercado em criptomoedas.',
     image: CryptoFolio,
+    status: 'Concluído',
     tags: [
       'NextJS',
       'TailwindCSS',
@@ -26,65 +33,108 @@ const projects = [
     link: 'https://crypto-folio-beta.vercel.app/',
     github: 'https://github.com/andersonwea/crypto-folio',
   },
+  {
+    name: 'Play ai',
+    description:
+      'O projeto consiste em um player de músicas que reflete de forma visual tudo que está tocando no momento, você pode pesquisar por qualquer música que esteja disponível no youtube.',
+    image: PlayAi,
+    status: 'Concluído',
+    tags: [
+      'ReactJs',
+      'TypeScript',
+      'TailwindCSS',
+      'p5.js',
+      'NodeJs',
+      'express',
+    ],
+    link: 'https://play-ai.vercel.app',
+    github: 'https://github.com/andersonwea/play-ai',
+  },
+  {
+    name: 'Mine it',
+    description:
+      'O Mine-It é um jogo simples onde o objetivo é encontrar os 8 diamantes sem encontrar a bomba. O jogo é jogado em um tabuleiro de 9x9 quadrados, e cada quadrado é uma mina que pode conter um diamante ou uma bomba.',
+    image: MineIt,
+    status: 'Concluído',
+    tags: ['ReactJs', 'Vite'],
+    link: 'https://andersonwea.github.io/mine-it/',
+    github: 'https://github.com/andersonwea/mine-it',
+  },
 ]
 
 export function Projects() {
+  const [currentSlider, setCurrentSlider] = React.useState(0)
+  const [loaded, setLoaded] = React.useState(false)
+  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
+    initial: 0,
+    slideChanged(slider) {
+      setCurrentSlider(slider.track.details.rel)
+    },
+    created() {
+      setLoaded(true)
+    },
+  })
+
+  function handleNext() {
+    instanceRef.current?.next()
+  }
+
+  function handlePrev() {
+    instanceRef.current?.prev()
+  }
+
   return (
     <section id="projets">
       <Title title="Meus projetos" />
 
-      <div className="grid grid-cols-2 mt-16">
-        <div>
-          <Image src={CryptoFolio} alt="" quality={100} />
-        </div>
-
-        <div className="pl-10">
-          <Text className="text-[rgba(77,181,255,0.2)] font-tertiary text-[150px] leading-none -ml-10">
-            01
-          </Text>
-          <Heading className="text-5xl leading-none -mt-[64px]">
-            Crypto Folio
-          </Heading>
-
-          <div className="mt-6">
-            <div className="flex gap-2 flex-wrap max-w-lg mt-5">
-              {projects.map((project, index) => {
-                return project.tags.map((tag) => (
-                  <Chip key={index} variant="bordered" color="primary">
-                    {tag}
-                  </Chip>
-                ))
-              })}
-            </div>
-          </div>
-
-          <Text as="p" className="mt-6">
-            {projects[0].description}
-          </Text>
-
-          <div className="flex items-center gap-5 mt-5">
-            <Button
-              as={Link}
-              href={projects[0].github}
-              isExternal
-              startContent={<Github />}
-              variant="ghost"
-              color="secondary"
-            >
-              Github
-            </Button>
-            <Button
-              as={Link}
-              href={projects[0].link}
-              isExternal
-              startContent={<ExternalLinkIcon />}
-              color="primary"
-            >
-              Acesse o site
-            </Button>
-          </div>
-        </div>
+      <div ref={sliderRef} className="keen-slider">
+        {projects.map((project, index) => (
+          <Projetc
+            key={project.name + index}
+            index={index + 1}
+            project={project}
+          />
+        ))}
       </div>
+
+      {loaded && instanceRef.current && (
+        <div className="mx-auto max-w-max">
+          <div className="flex gap-2 items-center mt-6">
+            <Button
+              color="primary"
+              isIconOnly
+              variant="light"
+              onClick={handlePrev}
+              disabled={currentSlider === 0}
+            >
+              <ChevronLeft />
+            </Button>
+
+            <div className="space-x-1">
+              <Text className="text-2xl text-white">
+                {(currentSlider + 1).toString()}
+              </Text>
+              <Text className="text-secondary font-bold">/</Text>
+              <Text className="text-secondary font-bold">
+                {projects.length.toString()}
+              </Text>
+            </div>
+
+            <Button
+              color="primary"
+              isIconOnly
+              variant="light"
+              onClick={handleNext}
+              disabled={
+                currentSlider ===
+                instanceRef.current.track.details.slides.length - 1
+              }
+            >
+              <ChevronRight />
+            </Button>
+          </div>
+        </div>
+      )}
     </section>
   )
 }
