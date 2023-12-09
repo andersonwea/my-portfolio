19e6849b6e1787ac75a3730b03283437b760f791
chain <klaus.mika1052@gmail.com>
2023-12-09 14:41:48 -0300

    feat: create project component

---
+++ src/app/components/Project.tsx
@@ -0,0 +1,88 @@
+import { Chip, Button, Link } from '@nextui-org/react'
+import { Github, ExternalLinkIcon } from 'lucide-react'
+import Image from 'next/image'
+import { Text } from './Text'
+import React from 'react'
+import { StaticImport } from 'next/dist/shared/lib/get-img-props'
+import { Heading } from './Heading'
+import 'keen-slider/keen-slider.min.css'
+
+type Project = {
+  name: string
+  description: string
+  image: StaticImport
+  status: string
+  tags: string[]
+  link: string
+  github: string
+}
+
+interface ProjectProps {
+  project: Project
+  index: number
+}
+
+export function Projetc({ project, index }: ProjectProps) {
+  return (
+    <div className="grid grid-cols-2 mt-16 keen-slider__slide">
+      <div className="rounded-2xl">
+        <Image
+          src={project.image}
+          alt=""
+          quality={100}
+          className="rounded-2xl w-full h-full object-cover"
+        />
+      </div>
+
+      <div className="pl-10">
+        <Text className="text-[rgba(77,181,255,0.2)] font-tertiary text-[150px] leading-none -ml-10">
+          {index.toString().padStart(2, '0')}
+        </Text>
+        <div className="flex gap-3 items-center -mt-[64px]">
+          <Heading className="text-5xl leading-none ">{project.name}</Heading>
+          <Chip variant="solid" className="bg-lime-500">
+            {project.status}
+          </Chip>
+        </div>
+
+        <div className="mt-6">
+          <div className="flex gap-2 flex-wrap max-w-lg mt-5">
+            {project.tags.map((tag, index) => {
+              return (
+                <Chip key={tag + index} variant="bordered" color="primary">
+                  {tag}
+                </Chip>
+              )
+            })}
+          </div>
+        </div>
+
+        <Text as="p" className="mt-6">
+          {project.description}
+        </Text>
+
+        <div className="flex items-center gap-5 mt-5">
+          <Button
+            as={Link}
+            href={project.github}
+            isExternal
+            startContent={<Github />}
+            variant="ghost"
+            color="secondary"
+          >
+            Github
+          </Button>
+          <Button
+            as={Link}
+            href={project.link}
+            isExternal
+            startContent={<ExternalLinkIcon />}
+            color="primary"
+          >
+            Acesse o site
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+}
